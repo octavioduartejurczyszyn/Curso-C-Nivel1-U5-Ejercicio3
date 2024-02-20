81c68709f45856aba441b48cc4cf1b94bb3feaae
octavioduartejurczyszyn <tatoduarte9@gmail.com>
2024-02-20 13:33:18 +0100

    Primera subida

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // 3. Hacer un programa que solicite 20 edades y luego calcule el
+           // promedio de edad de aquellas personas mayores a 18 años.
+            int edad, promedio, acu = 0, con = 0;
+            for (int x = 0; x < 20; x++)
+            {
+                Console.WriteLine("Escriba una edad: ");
+                edad = int.Parse(Console.ReadLine());
+                if (edad > 18)
+                {  
+                    acu += edad;
+                    con++;
+                }
+            }
+                promedio = acu / con;
+                Console.WriteLine("El promedio de edad es: " + promedio);
+        }
+    }
+}
